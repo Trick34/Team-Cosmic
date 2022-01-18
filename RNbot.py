9aa8afa93e1c2a402085fe9db30d884642aec006
Trick34 <95221485+Trick34@users.noreply.github.com>
2022-01-18 19:39:57 +0530

    Update RNbot.py

--- RNbot.py
+++ RNbot.py
@@ -149,7 +149,7 @@ async def bints(event):
         first_name = sender.first_name 
         xx = sender.id
         await event.edit(f"__Hey [{first_name}](tg://user?id={xx})  You Have Sucessfully__ applied for `Hacking/cc checker maker` __for our Community!__\n\n**We need to check your Skills.! PLEASE Come to** [Skills Tester MODERATORS](https://t.me/TC_ENTRY) **For Test**")
-        tosend = f"Hey MODERATORS [{first_name}](tg://user?id={xx}) Has Applied for `Bin Tester` Skill\n\nPlease Have Test of 's Skills!\n\nUSER INFO:\n•ID: {xx}\n•Name: {first_name}\n•Username: {sender.username}•\nProfile Link: [Link](tg://user?id={xx})"
+        tosend = f"Hey MODERATORS [{first_name}](tg://user?id={xx}) Has Applied for `Hacking/cc checker maker` Skill\n\nPlease Have Test of 's Skills!\n\nUSER INFO:\n•ID: {xx}\n•Name: {first_name}\n•Username: {sender.username}•\nProfile Link: [Link](tg://user?id={xx})"
         await bot.send_message(FRWD_GP, tosend)
 
 @bot.on(events.callbackquery.CallbackQuery(data="ckr"))
@@ -167,7 +167,7 @@ async def dev(event):
         first_name = sender.first_name 
         xx = sender.id
         await event.edit(f"__Hey [{first_name}](tg://user?id={xx})  You Have Sucessfully__ applied for `Developer/dumper` __for our Community!__\n\n**We need to check your Skills.! PLEASE Come to** [Skills Tester MODERATORS](https://t.me/TC_ENTRY) **For Test**")
-        tosend = f"Hey MODERATORS [{first_name}](tg://user?id={xx}) Has Applied for `Developer` Skill\n\nPlease Have Test of [{first_name}](tg://user?id={xx}) 's Skills!\n\nUSER INFO:\n•ID: {xx}\n•Name: {first_name}\n•Username: {sender.username}•\nProfile Link: [Link](tg://user?id={xx})"
+        tosend = f"Hey MODERATORS [{first_name}](tg://user?id={xx}) Has Applied for `Developer/dumper` Skill\n\nPlease Have Test of [{first_name}](tg://user?id={xx}) 's Skills!\n\nUSER INFO:\n•ID: {xx}\n•Name: {first_name}\n•Username: {sender.username}•\nProfile Link: [Link](tg://user?id={xx})"
         await bot.send_message(FRWD_GP, tosend)
 
 @bot.on(events.callbackquery.CallbackQuery(data="dokr"))
@@ -185,7 +185,7 @@ async def ccmake(event):
         first_name = sender.first_name 
         xx = sender.id
         await event.edit(f"__Hey [{first_name}](tg://user?id={xx})  You Have Sucessfully__ applied for `Config maker/combo maker` __for our Community!__\n\n**We need to check your Skills.! PLEASE Come to** [Skills Tester MODERATORS](https://t.me/TC_ENTRY) For Test**.")
-        tosend = f"Hey MODERATORS [{first_name}](tg://user?id={xx}) Has Applied for `CCMaker` Skill\n\nPlease Have Test of [{first_name}](tg://user?id={xx}) 's Skills!\n\nUSER INFO:\n•ID: {xx}\n•Name: {first_name}\n•Username: {sender.username}•\nProfile Link: [Link](tg://user?id={xx})"
+        tosend = f"Hey MODERATORS [{first_name}](tg://user?id={xx}) Has Applied for `Config maker/combo maker` Skill\n\nPlease Have Test of [{first_name}](tg://user?id={xx}) 's Skills!\n\nUSER INFO:\n•ID: {xx}\n•Name: {first_name}\n•Username: {sender.username}•\nProfile Link: [Link](tg://user?id={xx})"
         await bot.send_message(FRWD_GP, tosend)
 
 @bot.on(events.callbackquery.CallbackQuery(data="ccspmr"))
@@ -194,7 +194,7 @@ async def spmr(event):
         first_name = sender.first_name 
         xx = sender.id
         await event.edit(f"__Hey [{first_name}](tg://user?id={xx})  You Have Sucessfully__ applied for `CC Spammer/bomber` __for our Community!__\n\n**We need to check your Skills.! PLEASE Come to** [Skills Tester MODERATORS](https://t.me/TC_ENTRY) For Test**.")
-        tosend = f"Hey MODERATORS [{first_name}](tg://user?id={xx}) Has Applied for `CC Spammer` Skill\n\nPlease Have Test of [{first_name}](tg://user?id={xx}) 's Skills!\n\nUSER INFO:\n•ID: {xx}\n•Name: {first_name}\n•Username: {sender.username}•\nProfile Link: [Link](tg://user?id={xx})"
+        tosend = f"Hey MODERATORS [{first_name}](tg://user?id={xx}) Has Applied for `CC Spammer/bomber` Skill\n\nPlease Have Test of [{first_name}](tg://user?id={xx}) 's Skills!\n\nUSER INFO:\n•ID: {xx}\n•Name: {first_name}\n•Username: {sender.username}•\nProfile Link: [Link](tg://user?id={xx})"
         await bot.send_message(FRWD_GP, tosend)
         
     # <--User Message frwdr--> 
